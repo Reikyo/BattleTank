25ea2e183009fc03ba0e15bbb440d6b7e79400bf
Darren Temple <Kyo@192.168.1.28>
2017-02-18 21:28:44 -0800

    Video 134: Refactored code to use forward declarations throughout

--- BattleTank/Source/BattleTank/TankBarrel.h
+++ BattleTank/Source/BattleTank/TankBarrel.h
@@ -5,9 +5,7 @@
 #include "Components/StaticMeshComponent.h"
 #include "TankBarrel.generated.h"
 
-/**
- * 
- */
+
 // Hide categories from the Blueprint "Details" tab by e.g.:
 UCLASS( meta=(BlueprintSpawnableComponent), hidecategories=("Collision") )
 class BATTLETANK_API UTankBarrel : public UStaticMeshComponent
